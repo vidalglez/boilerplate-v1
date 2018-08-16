6c94f6d23b01e9f55df9942b3e4b189f47a4956c
Vidal Gonzalez <darkvidal@gmail.com>
2018-08-15 19:56:21 -0500

    boilerplate-v2

---
+++ src/components/Header.js
@@ -0,0 +1,24 @@
+import React from 'react';
+import ReactDOM from 'react-dom';
+import { Link } from 'react-router-dom';
+import { connect } from 'react-redux';
+import { startLogout } from '../actions/auth';
+
+export const Header = ({ startLogout }) => (
+  <header className="header">
+    <div className="content-container">
+      <div className="header_content">
+        <Link className="header__title" to="/dashboard" >
+          <h1>Boilertplate React App</h1>
+        </Link>
+        <button className="button button__link" onClick={startLogout}>Logout</button>
+      </div>
+    </div>
+  </header>
+);
+
+const mapDispatchToProps = (dispatch) => ({
+  startLogout: () => dispatch(startLogout())
+});
+
+export default connect(undefined, mapDispatchToProps)(Header);
